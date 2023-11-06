53fbbc33087e9bab17f191d3774af0f250f2d108
NadimBhuiyan34 <nadimbhuiyan15900@gmail.com>
2023-11-06 11:43:54 +0600

    advertise api

---
+++ searchapi/get_advertise.php
@@ -0,0 +1,31 @@
+<?php
+require '../config.php';
+if($_POST['verify'] == 'advertise')
+{
+     
+   
+        $fetchQuery = "SELECT * FROM `advertises`";
+
+        $advertises = mysqli_query($connection, $fetchQuery );
+
+        if($advertises)
+        {
+            $data = array();
+          while ($advertise = mysqli_fetch_assoc($advertises)) {
+            $data[] = $advertise;
+        }
+        }
+        else
+        {
+            $data = [
+                'message' => "Something is wrong",
+            ]; 
+        }
+   
+    
+   
+    header('Content-Type: application/json');
+    echo json_encode($data);
+
+}
+?>
